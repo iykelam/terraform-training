a82096d610325571c42fb21e650767ea0ca2e51d
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2023-12-20 19:27:37 -0500

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -3,6 +3,7 @@ terraform {
 	backend "s3" {
  		encrypt = true
  		bucket = "terraform-remote-state-july-ibt"
+		dynamodb_table = "terraform-state-lock-dynamo"
 		region = "us-west-2"
  		key = "terraform.tfstate"
  }
