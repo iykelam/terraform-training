6d403cbfd9982091ad2b812a681013610127d09c
iykelam <108640707+iykelam@users.noreply.github.com>
2023-12-31 10:38:50 -0800

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -3,7 +3,7 @@ terraform {
 	backend "s3" {
  		encrypt = true
  		bucket = "terraform-remote-state-july-ibt"
-		dynamodb_table = "terraform-state-lock-dynamo"
+		dynamodb_table = "terraform-state-lock-dynamo"     #Using this prevents errors in pulling/creating outdated files into terraform.tfstate
 		region = "us-west-2"
  		key = "terraform.tfstate"
  }
